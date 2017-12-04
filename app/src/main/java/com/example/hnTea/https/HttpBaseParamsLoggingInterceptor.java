36f3d6124361fdc34acc6e6c6f33068adfd4ab86
Administrator <1916973618@qq.com>
2017-12-04 16:19:51 +0800

    豫茶

---
+++ app/src/main/java/com/example/hnTea/https/HttpBaseParamsLoggingInterceptor.java
@@ -0,0 +1,9 @@
+package com.example.hnTea.https;
+
+/**
+ * Created by Administrator on 2017/6/15 0015.
+ */
+
+public class HttpBaseParamsLoggingInterceptor {
+//        implements Interceptor {
+}
